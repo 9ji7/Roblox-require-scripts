b8fdb0bef43b345909bacf10ac6b06b9dbe183f4
9ji7 <beatmaker60608@gmail.com>
2025-07-21 01:34:32 +0300

    Create Lookinsidetherearerequirescripts.lua
    Created requires

---
+++ Lookinsidetherearerequirescripts.lua
@@ -0,0 +1,5 @@
+knife grab(if i remember): require(18665717275).load("usernamehere")
+the c00lkid from forsaken: require(89709417365094).Forsaken("user", "c00lkidd")
+  i dont remember what is this: require(18665717275).load("usernamehere")
+  for now all
+  if the last one destructive ili delete it just warn me
