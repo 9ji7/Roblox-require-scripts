be8b0961b716471ee840c26562d2384e3694c854
9ji7 <beatmaker60608@gmail.com>
2025-08-23 03:37:59 +0300

    Update Lookinsidetherearerequirescripts.lua

--- Lookinsidetherearerequirescripts.lua
+++ Lookinsidetherearerequirescripts.lua
@@ -2,3 +2,10 @@ knife grab(if i remember): require(18665717275).load("usernamehere")
 the c00lkid from forsaken: require(89709417365094).Forsaken("user", "c00lkidd")
   for now all
   if the last one destructive ili delete it just warn me
+require(4722146433).load("putyourrobloxusernamehere")
+ 
+Controls: R,E,Q,Z, Tp X,M,H,G,F,Y,T,E
+    an interstekkar glitcher
+    fun one
+    some sounds are missing
+    and btw when u switch to another glitcher dont be scaried of the sound!:)
